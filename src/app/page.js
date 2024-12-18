7979f5a5e158839207ed2c7c6852717d1be1de6b
RoystonDAlmeida <roystonjan1@hotmail.com>
2024-12-19 02:13:08 +0530

    Added default path for router on refreshing

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,29 @@
 // app/page.js
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import Header from "./Components/Header"
-import RoundedBox from "./Components/RoundedBox";
-import LocationAccess from "./LocationAccess/LocationAccess";
+import LocationManager from "./Components/LocationManager";
 
 export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    // Check if location query parameter exists
+    const locationQuery = new URLSearchParams(window.location.search).get('location');
+    if (locationQuery) {
+      // Redirect to home page
+      router.push('/');
+    }
+  }, [router]); // Depend on router
+
   return (
     <div>
       <Header/>
       <main className="main">
-          {/* Include the Full Width Box */}
-          <RoundedBox />
-                
-          {/* Include the Location Access component */}
-          <LocationAccess />
+           {/* Include the Location Manager component(contains search bar) */}
+          <LocationManager />
       </main>
     </div>
   );
