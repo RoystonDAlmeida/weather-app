2e92db43265d392bce98d1f6c8078d4add20080b
RoystonDAlmeida <roystonjan1@hotmail.com>
2024-12-19 02:28:28 +0530

    Getting country flag emoji util based on country code

---
+++ src/app/utils/getCountryFlagEmoji.js
@@ -0,0 +1,9 @@
+// utils/getCountryFlagEmoji.js
+
+export const getCountryFlagEmoji = (countryCode) => {
+    const codePoints = countryCode
+        .toUpperCase()
+        .split('')
+        .map((char) => 127397 + char.charCodeAt(0));
+    return String.fromCodePoint(...codePoints);
+};
